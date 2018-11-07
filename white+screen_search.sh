57567d16dd1d05d9c20350e72c1d7ae007c76495
Ads <ads@iac>
2018-11-07 15:39:49 +0300

    Add project

---
+++ white+screen_search.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+hosts=`cat image`
+for i in $hosts
+do
+ping $i -c 1
+if [ $? -eq 0 ]; then
+name=`sshpass -p 'Zxcv!234' ssh -q -o 'UserKnownHostsFile /dev/null' -o 'StrictHostKeyChecking=no' root@$i "hostname"`
+sshpass -p 'Zxcv!234' ssh -q -o 'UserKnownHostsFile /dev/null' -o 'StrictHostKeyChecking=no' root@$i "export DISPLAY=:0"
+sshpass -p 'Zxcv!234' ssh -q -o 'UserKnownHostsFile /dev/null' -o 'StrictHostKeyChecking=no' root@$i "DISPLAY=:0 import -window root /tmp/$name.png"
+sshpass -p 'Zxcv!234' scp -o StrictHostKeyChecking=no root@$i:/tmp/*.png /home/ads/log
+else
+echo "The host $i is down!"
+fi
+done
+
+#if [ $? -eq 0 ]; then
+#fz=``find /home/ads/log -type f -size -50k -exec ls -S -l -h {} \;
+#printf '%s\n' "$fz"
+#fi
+
+if [ $? -eq 0 ]; then
+find /home/ads/log -type f -size -16k -exec cp {} /home/ads/333/ \;
+ls /home/ads/333/ | sed 's/.png//g' >> white_screen.txt
+fi
